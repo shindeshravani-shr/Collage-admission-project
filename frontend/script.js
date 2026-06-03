22602292f46395a294083484bc4dce052c661046
shindeshravani-shr <shindeshravani1984@gmail.com>
2026-06-03 16:35:05 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,25 @@
 document.getElementById("admissionForm")
-.addEventListener("submit", async(e)=>{
+.addEventListener("submit", async (e) => {
 
-e.preventDefault();
+    e.preventDefault();
 
-alert("Form Submitted");
+    const data = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        course: document.getElementById("course").value
+    };
 
+    const res = await fetch("http://EC2-IP:5000/admission", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const msg = await res.text();
+
+    alert(msg);   // backend response दाखवेल
+
+    document.getElementById("admissionForm").reset();
 });
